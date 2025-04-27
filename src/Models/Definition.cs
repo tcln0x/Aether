0909d2d6551284f7e2bc3b4713c53b9958edfc8e
tcnl <208707452+tcln0x@users.noreply.github.com>
2025-04-27 21:45:14 +0300

    Updated definition model
    Retries, delays, headers and user agent added.

--- src/Models/Definition.cs
+++ src/Models/Definition.cs
@@ -8,18 +8,30 @@ namespace Aether.Models
 
         public string Name { get; set; }
 
-        public string Template { get; set; }
+        public string UrlTemplate { get; set; }
+
+        public string? UrlMain { get; set; }
+
+        public int? Rank { get; set; }
 
         public CheckStrategy Strategy { get; set; }
 
-        public List<int> MatchedCodes { get; set; } = [];
+        public List<int> PositiveStatus { get; set; } = [];
 
-        public List<int> UnmatchedCodes { get; set; } = [];
+        public List<int> NegativeStatus { get; set; } = [];
 
-        public List<string> MatchedStrings { get; set; } = [];
+        public List<string> PresenceStrings { get; set; } = [];
 
-        public List<string> UnmatchedStrings { get; set; } = [];
+        public List<string> AbsenceStrings { get; set; } = [];
+
+        public Dictionary<string, string> Headers { get; set; } = [];
+
+        public string? ExtractPattern { get; set; }
 
         public List<string> Tags { get; set; } = [];
+
+        public int DelayMs { get; set; } = 0;
+
+        public int Retries { get; set; } = 3;
     }
 }
